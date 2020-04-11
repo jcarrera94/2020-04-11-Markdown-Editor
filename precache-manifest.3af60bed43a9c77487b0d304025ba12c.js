6868ea148ee4409f93f5fffcaa34a6183c93694d
Juan Carrera <juankarrera@gmail.com>
2020-04-11 15:04:21 -0400

    Updates

---
+++ precache-manifest.3af60bed43a9c77487b0d304025ba12c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07d1d794e827c69716afc652d875940b",
+    "url": "/2020-04-11-Markdown-Editor/index.html"
+  },
+  {
+    "revision": "41ca038000bc2c0fdf7a",
+    "url": "/2020-04-11-Markdown-Editor/static/css/main.70a259e3.chunk.css"
+  },
+  {
+    "revision": "411ba837158e9e79726f",
+    "url": "/2020-04-11-Markdown-Editor/static/js/2.0c78cf47.chunk.js"
+  },
+  {
+    "revision": "7a29fd67914f99c562dc3f65bbf1078b",
+    "url": "/2020-04-11-Markdown-Editor/static/js/2.0c78cf47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41ca038000bc2c0fdf7a",
+    "url": "/2020-04-11-Markdown-Editor/static/js/main.c28f23c2.chunk.js"
+  },
+  {
+    "revision": "ca479e68d955670e7c3c",
+    "url": "/2020-04-11-Markdown-Editor/static/js/runtime-main.f691fa90.js"
+  }
+]);
